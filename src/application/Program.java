7cdc11009e5e3617c9cd25fea5502c2c5f5637c2
Moises Felix <moises012felix@gmail.com>
2024-05-30 14:46:18 -0300

    reorganizando

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,29 +11,8 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = DB.getConnection();
-			
-			st = conn.createStatement();
-			
-			rs = st.executeQuery("select * from department");
-			
-			while(rs.next()) {
-				System.out.println(rs.getInt("Id")+ ", " +rs.getString("Name"));
-			}
-		}
-		catch(SQLException	e) {
-			e.printStackTrace();
-		}
-		finally {
-			DB.closeResultSet(rs);
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		Connection con = DB.getConnection();
+		DB.closeConnection();
 	}
 
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -4,13 +4,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
@@ -42,26 +39,6 @@ public class DB {
 				conn.close();
 			}
 			catch(SQLException e) {
-				throw new DbException(e.getMessage());
-			}
-		}
-	}
-	
-	public static void closeStatement(Statement st) {
-		if(st != null) {
-			try {
-				st.close();
-			} catch (SQLException e) {
-				throw new DbException(e.getMessage());
-			}
-		}
-	}
-	
-	public static void closeResultSet(ResultSet rs) {
-		if(rs != null) {
-			try {
-				rs.close();
-			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
